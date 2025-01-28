abbbca0ae421b22d18ae1d4adebcd75508a732b5
haruka kinami <pooh.pooh.po0oh.38@gmail.com>
2025-01-28 19:31:15 +0900

    add:kinami:微修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,11 +1,7 @@
 class MypagesController < ApplicationController
   def index
-    @books = Book.where(status:"reading").select(:title, :author, :pages)
-    @goals = Goal
-    .joins(:book)
+    @books = current_user.books.where(status:"reading").select(:title, :author, :pages)
+    @goals = current_user.goals.includes(:book)
     .where(status:"unachieved")
-    #current_pageを入れる
-    #.select("books.title, start_date, target_date, pages_per_day, current_page")
-    .select("books.title, start_date, target_date, pages_per_day")
   end
 end
