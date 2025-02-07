beb664a33aa4d2562c20272c67a262ac1e544556
haruka kinami <pooh.pooh.po0oh.38@gmail.com>
2025-02-07 20:54:44 +0900

    add:kinami:'Bootstrap5で見た目を整える'まで

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: %i[ show edit update destroy ]
+
+  # GET /books or /books.json
+  def index
+    @search = current_user.books.ransack(params[:q])
+    #エラーになる
+    #@search.sorts = 'id desc' if @search.sorts.empty?
+    @books = @search.result.page(params[:page])
+  end
+
+  # GET /books/1 or /books/1.json
+  def show
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books or /books.json
+  def create
+    @book = current_user.books.new(book_params)
+    if @book.save
+      redirect_to @book, notice: "本を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  # PATCH/PUT /books/1 or /books/1.json
+  def update
+    if @book.update
+      redirect_to @book, notice: "本を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/1 or /books/1.json
+  def destroy
+    @book.destroy
+    redirect_to books_path, notice: "本を削除しました。"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title, :author, :pages, :status, :user)
+    end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,19 @@ class Book < ApplicationRecord
   has_many :goals, dependent: :destroy
   belongs_to :user
 
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :pages, presence: true
+  validates :status, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["title", "author", "status"]
+  end
+
+  def self.ransortable_attributes(auth_object = nil)
+    ransackable_attributes(auth_object)
+  end
+
   # purchase:   購入予定
   # possession: 所持
   # reading:    読書中
